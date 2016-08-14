02918ebd9b39ac58271484a95da9359c6478d746
王俊超 <shining-glory@qq.com>
2016-08-15 06:21:01 +0800

    commit

---
+++ 007-添加类成员/src/AddFieldAdapter.java
@@ -0,0 +1,44 @@
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+
+import static org.objectweb.asm.Opcodes.ASM4;
+
+/**
+ * Author: Íõ¿¡³¬
+ * Time: 2016-08-14 16-57 10:58
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ */
+public class AddFieldAdapter extends ClassVisitor {
+    private int fAcc;
+    private String fName;
+    private String fDesc;
+    private boolean isFieldPresent;
+
+    public AddFieldAdapter(ClassVisitor cv, int fAcc, String fName, String fDesc) {
+        super(ASM4, cv);
+        this.fAcc = fAcc;
+        this.fName = fName;
+        this.fDesc = fDesc;
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+        if (name.equals(fName)) {
+            isFieldPresent = true;
+        }
+        return cv.visitField(access, name, desc, signature, value);
+    }
+
+    @Override
+    public void visitEnd() {
+        if (!isFieldPresent) {
+            FieldVisitor fv = cv.visitField(fAcc, fName, fDesc, null, null);
+            if (fv != null) {
+                fv.visitEnd();
+            }
+        }
+        cv.visitEnd();
+    }
+}
